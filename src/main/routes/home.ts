fe432d5b60adb5705d331fea1ce7d717a053e978
Owen A Steel <woofdog04@gmail.com>
2025-09-17 18:32:01 +0100

    Basic fetch and display for case tasks

--- src/main/routes/home.ts
+++ src/main/routes/home.ts
@@ -2,11 +2,22 @@ import { Application } from 'express';
 import axios from 'axios';
 
 export default function (app: Application): void {
-  app.get('/', async (req: any, res: { render: (arg0: string, arg1: { example?: any; }) => void; }) => {
+  app.get('/', async (req: any, res: {
+    render: (arg0: string, arg1: {
+      case?: any;
+      tasks?: any;
+    }) => void;
+  }) => {
     try {
-      const response = await axios.get('http://localhost:4000/api/cases/get-example-case');
-      console.log(response.data);
-      res.render('home', { "example": response.data });
+      const caseDetailsResponse = await axios.get('http://localhost:4000/api/cases/get-example-case');
+      const caseTasksResponse = await axios.get(`http://localhost:4000/api/cases/${caseDetailsResponse.data.id}/tasks`);
+      res.render(
+        'home',
+        {
+          case: caseDetailsResponse.data,
+          tasks: caseTasksResponse.data
+        }
+      );
     } catch (error) {
       console.error('Error making request:', error);
       res.render('home', {});
