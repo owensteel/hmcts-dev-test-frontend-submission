06d4da662c8f5dcdf795253f2738e531bbd11536
Owen A Steel <woofdog04@gmail.com>
2025-09-17 12:15:32 +0100

    Uses new endpoint

--- src/main/routes/home.ts
+++ src/main/routes/home.ts
@@ -4,8 +4,7 @@ import axios from 'axios';
 export default function (app: Application): void {
   app.get('/', async (req: any, res: { render: (arg0: string, arg1: { example?: any; }) => void; }) => {
     try {
-      // An example of connecting to the backend (a starting point)
-      const response = await axios.get('http://localhost:4000/get-example-case');
+      const response = await axios.get('http://localhost:4000/api/cases/get-example-case');
       console.log(response.data);
       res.render('home', { "example": response.data });
     } catch (error) {
