8532372f46b35dbd0794c73cc8b60996f2b61101
Owen A Steel <woofdog04@gmail.com>
2025-09-22 20:21:41 +0100

    Implements get and update endpoints for Task Service

--- src/services/TaskService.ts
+++ src/services/TaskService.ts
@@ -1,4 +1,4 @@
-import { Task, TaskCreate } from '../models/Task';
+import { Task, TaskCreate, TaskUpdate } from '../models/Task';
 import { ApiClient } from '../services/ApiClient';
 
 import { isAxiosError } from 'axios';
@@ -12,11 +12,36 @@ export class TaskService {
 
   async create(caseId: number, task: TaskCreate): Promise<Task> {
     try {
-      return await this.apiClient.saveOrCreateTask(caseId, {
+      return await this.apiClient.createTask(caseId, {
         title: task.title,
         description: task.description || '',
         dueDateTime: new Date(task.dueDateTime).toISOString(),
       });
+    } catch (e) {
+      if (isAxiosError(e)) {
+        throw e;
+      }
+      throw new Error(String(e));
+    }
+  }
+
+  async get(taskId: number): Promise<Task> {
+    try {
+      return await this.apiClient.getTask(taskId);
+    } catch (e) {
+      if (isAxiosError(e)) {
+        throw e;
+      }
+      throw new Error(String(e));
+    }
+  }
+
+  async update(taskId: number, updates: TaskUpdate): Promise<Task> {
+    try {
+      if (updates.dueDateTime) {
+        updates.dueDateTime = new Date(updates.dueDateTime).toISOString();
+      }
+      return await this.apiClient.updateTask(taskId, updates);
     } catch (e) {
       if (isAxiosError(e)) {
         throw e;
