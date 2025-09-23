fc82750fe5c4f8b62909b2d646ab023315e46f38
Owen A Steel <woofdog04@gmail.com>
2025-09-23 18:24:50 +0100

    Validates Task Edit route

--- src/main/routes/tasks.ts
+++ src/main/routes/tasks.ts
@@ -15,6 +15,14 @@ const TASK_STATUS_MAP_TO_USER_FRIENDLY_VALUES: Record<string, string> = {
   [TaskStatus.BLOCKED]: 'Blocked',
 };
 
+const TASK_EDIT_VALID_HIGHLIGHTED_PROPERTIES: string[] = [
+  'delete-description',
+  'title',
+  'description',
+  'due-date',
+  'status',
+];
+
 export default function (app: Application): void {
   const apiClient = new ApiClient();
   const taskService = new TaskService();
@@ -70,26 +78,31 @@ export default function (app: Application): void {
   app.get('/tasks/:taskId/edit/:highlightedProperty', async (req, res) => {
     const { taskId, highlightedProperty } = req.params;
 
-    try {
-      const taskToEdit = await taskService.get(parseInt(taskId));
-      res.render('../views/tasks/edit.njk', {
-        taskId,
-        errors: null,
-        // Values to display in the input(s)
-        values: {
-          title: taskToEdit.title,
-          description: taskToEdit.description,
-          status: taskToEdit.status,
-        },
-        // Original values that will remain as
-        // what is actually in the database
-        originalValues: {
-          title: taskToEdit.title,
-        },
-        highlightedProperty,
-      });
-    } catch (e) {
-      res.status(404).render('not-found');
+    // Check that the specified property to edit is valid
+    if (!TASK_EDIT_VALID_HIGHLIGHTED_PROPERTIES.includes(highlightedProperty)) {
+      res.status(500).render('error');
+    } else {
+      try {
+        const taskToEdit = await taskService.get(parseInt(taskId));
+        res.render('../views/tasks/edit.njk', {
+          taskId,
+          errors: null,
+          // Values to display in the input(s)
+          values: {
+            title: taskToEdit.title,
+            description: taskToEdit.description,
+            status: taskToEdit.status,
+          },
+          // Original values that will remain as
+          // what is actually in the database
+          originalValues: {
+            title: taskToEdit.title,
+          },
+          highlightedProperty,
+        });
+      } catch (e) {
+        res.status(404).render('not-found');
+      }
     }
   });
 
@@ -97,47 +110,52 @@ export default function (app: Application): void {
   app.post('/tasks/:taskId/edit/:highlightedProperty', async (req, res) => {
     const { taskId, highlightedProperty } = req.params;
 
-    const taskUpdateForm: TaskUpdateForm = new TaskUpdateForm();
-    // Add properties to form depending on what is submitted
-    if ('title' in req.body) {
-      taskUpdateForm.title = req.body.title;
-    }
-    if ('description' in req.body) {
-      taskUpdateForm.description = req.body.description;
-    }
-    if ('status' in req.body) {
-      taskUpdateForm.status = req.body.status;
-    }
-    if ('due-date-time-year' in req.body && 'due-date-time-month' in req.body && 'due-date-time-day' in req.body) {
-      const dateInputsCombined = `${req.body['due-date-time-year']}-${req.body['due-date-time-month'].padStart(2, '0')}-${req.body['due-date-time-day'].padStart(2, '0')}`;
-      taskUpdateForm.dueDateTime = dateInputsCombined;
-    }
-
-    // Stop and display validation errors, if any
-    const formValidationErrors = taskUpdateForm.validateAndGetErrors();
-    if (formValidationErrors.length > 0) {
-      // Make sure original values are up-to-date
-      const taskToEdit = await taskService.get(parseInt(taskId));
-      return res.render('../views/tasks/edit.njk', {
-        taskId,
-        errors: formValidationErrors,
-        // Values to display in the inputs, will now reflect
-        // user's submission by this point
-        values: req.body,
-        // Original values, what the task's data currently is
-        originalValues: {
-          title: taskToEdit.title,
-        },
-        highlightedProperty,
-      });
+    // Check that the specified property to edit is valid
+    if (!TASK_EDIT_VALID_HIGHLIGHTED_PROPERTIES.includes(highlightedProperty)) {
+      res.status(500).render('error');
     } else {
-      // Submit to backend
-      try {
-        await taskService.update(Number(taskId), taskUpdateForm);
-        res.redirect('/tasks/' + taskId + '/view');
-      } catch (error) {
-        console.error(error);
-        res.status(500).render('error');
+      const taskUpdateForm: TaskUpdateForm = new TaskUpdateForm();
+      // Add properties to form depending on what is submitted
+      if ('title' in req.body) {
+        taskUpdateForm.title = req.body.title;
+      }
+      if ('description' in req.body) {
+        taskUpdateForm.description = req.body.description;
+      }
+      if ('status' in req.body) {
+        taskUpdateForm.status = req.body.status;
+      }
+      if ('due-date-time-year' in req.body && 'due-date-time-month' in req.body && 'due-date-time-day' in req.body) {
+        const dateInputsCombined = `${req.body['due-date-time-year']}-${req.body['due-date-time-month'].padStart(2, '0')}-${req.body['due-date-time-day'].padStart(2, '0')}`;
+        taskUpdateForm.dueDateTime = dateInputsCombined;
+      }
+
+      // Stop and display validation errors, if any
+      const formValidationErrors = taskUpdateForm.validateAndGetErrors();
+      if (formValidationErrors.length > 0) {
+        // Make sure original values are up-to-date
+        const taskToEdit = await taskService.get(parseInt(taskId));
+        return res.render('../views/tasks/edit.njk', {
+          taskId,
+          errors: formValidationErrors,
+          // Values to display in the inputs, will now reflect
+          // user's submission by this point
+          values: req.body,
+          // Original values, what the task's data currently is
+          originalValues: {
+            title: taskToEdit.title,
+          },
+          highlightedProperty,
+        });
+      } else {
+        // Submit to backend
+        try {
+          await taskService.update(Number(taskId), taskUpdateForm);
+          res.redirect('/tasks/' + taskId + '/view');
+        } catch (error) {
+          console.error(error);
+          res.status(500).render('error');
+        }
       }
     }
   });
