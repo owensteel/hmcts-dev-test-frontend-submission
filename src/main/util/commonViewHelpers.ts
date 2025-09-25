01fb7bb925d3c5b2dd4e75c16b25993fdc1b7898
Owen A Steel <woofdog04@gmail.com>
2025-09-25 13:33:30 +0100

    Creates common view helpers for generation of elements and other generic utils

---
+++ src/main/util/commonViewHelpers.ts
@@ -0,0 +1,50 @@
+import {
+  getAllTaskStatusesWithUserFriendlyLabel,
+  getStatusTagClass,
+  getStatusUserFriendlyLabel,
+} from './taskViewHelpers';
+
+export function generateTaskStatusSelectorOptions(selectedStatus?: string): {
+  // Based on Nunjucks
+  value: string;
+  text: string;
+  selected: boolean;
+}[] {
+  const taskStatusSelectorOptions = [];
+  for (const status of getAllTaskStatusesWithUserFriendlyLabel()) {
+    taskStatusSelectorOptions.push({
+      value: status,
+      text: getStatusUserFriendlyLabel(status),
+      selected: selectedStatus === status,
+    });
+  }
+  return taskStatusSelectorOptions;
+}
+
+// GOV UK components generated server-side due to Nunjucks issues
+
+export function htmlGovUK_tagForStatus(taskStatus: string): string {
+  return `<strong class="govuk-tag ${getStatusTagClass(taskStatus)}">${getStatusUserFriendlyLabel(taskStatus)}</strong>`;
+}
+
+export function htmlGovUK_summaryListActionsList(
+  actions: {
+    text: string;
+    href: string;
+    visuallyHidden: string;
+  }[]
+): string {
+  let element = '<ul class="govuk-summary-list__actions-list">';
+  for (const action of actions) {
+    element += `
+        <li class="govuk-summary-list__actions-list-item">
+            <a class="govuk-link" href="${action.href}">
+                ${action.text}
+                <span class="govuk-visually-hidden">${action.visuallyHidden}</span>
+            </a>
+        </li>
+    `;
+  }
+  element += '</ul>';
+  return element;
+}
