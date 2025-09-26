d0a0f95fd9b6b56837af53b8a30b45fb1bb4b734
Owen A Steel <woofdog04@gmail.com>
2025-09-26 12:15:34 +0100

    Tests homepage and case display

--- src/test/integration/routes/home.test.ts
+++ src/test/integration/routes/home.test.ts
@@ -1,14 +1,39 @@
 import { app } from '../../../main/app';
+import { Case } from '../../../models/Case';
+import { ApiClient } from '../../../services/ApiClient';
 
-import { expect } from 'chai';
 import request from 'supertest';
 
+// Mock the whole ApiClient class
+jest.mock('../../../services/ApiClient');
+
+const mockedApiClient = ApiClient as jest.MockedClass<typeof ApiClient>;
+
+const fakeCaseData: Case = {
+  id: 1,
+  caseNumber: 'ABC12345',
+  title: 'Case Title',
+  description: 'Case description',
+  status: 'Case Status',
+  createdDate: new Date().toISOString(),
+  createdAt: '',
+};
+
 describe('Home page', () => {
-  describe('on GET', () => {
-    test('should return sample home page', async () => {
-      await request(app)
-        .get('/')
-        .expect(res => expect(res.status).to.equal(200));
-    });
+  beforeEach(() => {
+    jest.resetAllMocks();
+
+    // Fake implementation for getCase
+    mockedApiClient.prototype.getExampleCase.mockResolvedValue(fakeCaseData);
+  });
+
+  it('GET / should return 200 and render the case page and display case details', async () => {
+    const res = await request(app).get('/');
+    expect(res.status).toBe(200);
+    expect(res.text).toContain('Case details');
+    expect(res.text).toContain(fakeCaseData.title);
+    expect(res.text).toContain(fakeCaseData.description);
+    expect(res.text).toContain(fakeCaseData.status);
+    expect(res.text).toContain(fakeCaseData.createdDate);
   });
 });
