4d4ed88a8e16337bf5dbaf3dc49421c8690d06ec
Owen A Steel <woofdog04@gmail.com>
2025-09-27 13:54:34 +0100

    Adds docs to taskpage interface

--- src/models/TaskPage.ts
+++ src/models/TaskPage.ts
@@ -2,7 +2,9 @@ export interface TaskPage<T> {
   content: T[];
   totalElements: number;
   totalPages: number;
+  /** The amount of items requested per page */
   size: number;
+  /** The page index (0-index) */
   number: number;
   first: boolean;
   last: boolean;
